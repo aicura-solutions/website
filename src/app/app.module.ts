5d81c9ca6027a804b087e81398041672abbce844
Dan <iamdanhenderson@gmail.com>
2019-05-03 11:14:16 +0100

    Added homepage rough design with Aicura logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AicuraLogoComponent } from './aicura-logo/aicura-logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AicuraLogoComponent
   ],
   imports: [
     BrowserModule,
