15b58d028a0a99aec42beac160fd67051255a553
Dan <iamdanhenderson@gmail.com>
2019-05-17 08:58:42 +0100

    Added blue waves in above footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ComingSoonComponent } from './coming-soon/coming-soon.component';
 import { OpenSourceBankingComponent } from './open-source-banking/open-source-banking.component';
 import { FrontEndAutomationComponent } from './front-end-automation/front-end-automation.component';
 import { NavigationToggleComponent } from './navigation-toggle/navigation-toggle.component';
+import { NavigationToggleWhiteComponent } from './navigation-toggle-white/navigation-toggle-white.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { NavigationToggleComponent } from './navigation-toggle/navigation-toggle
     ComingSoonComponent,
     OpenSourceBankingComponent,
     FrontEndAutomationComponent,
-    NavigationToggleComponent
+    NavigationToggleComponent,
+    NavigationToggleWhiteComponent
   ],
   imports: [
     BrowserModule,
