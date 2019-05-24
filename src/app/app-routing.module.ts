4afcf4c5930b846eaa48e41a4e39b51f73610ad9
Dan <iamdanhenderson@gmail.com>
2019-05-24 15:05:53 +0100

    Added missing bracket

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,7 @@ const routes: Routes = [
   { path: 'about', component: ComingSoonComponent },
   { path: 'our-team', component: ComingSoonComponent },
   { path: '**', component: PageNotFoundComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
