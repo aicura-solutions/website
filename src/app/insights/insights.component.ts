2eb64a490de1fa576562cfe3434f78f1a5e311a5
Dan Henderson <danhenderson95@users.noreply.github.com>
2019-05-29 09:28:28 +0100

    Have updated insight page to include filters (#10)

--- src/app/insights/insights.component.ts
+++ src/app/insights/insights.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { all } from 'q';
 
 @Component({
   selector: 'app-insights',
@@ -6,10 +7,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./insights.component.scss']
 })
 export class InsightsComponent implements OnInit {
+  duckUrl = 'assets/img/aicura-duck.png';
+
+  noResults = false;
+
+  loadAll = true;
+  loadEvents = false;
+  loadPress = false;
+  loadInsights = false;
+  loadCaseStudies = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClickAll() {
+    this.loadAll = true;
+    this.loadEvents = false;
+    this.loadPress = false;
+    this.loadInsights = false;
+    this.loadCaseStudies = false;
+    this.noResults = false;
+  }
+
+  onClickEvents() {
+    this.loadAll = false;
+    this.loadEvents = true;
+    this.loadPress = false;
+    this.loadInsights = false;
+    this.loadCaseStudies = false;
+    this.noResults = false;
+  }
+  onClickPress() {
+    this.loadAll = false;
+    this.loadEvents = false;
+    this.loadPress = true;
+    this.loadInsights = false;
+    this.loadCaseStudies = false;
+    this.noResults = true;
+  }
+  onClickInsights() {
+    this.loadAll = false;
+    this.loadEvents = false;
+    this.loadPress = false;
+    this.loadInsights = true;
+    this.loadCaseStudies = false;
+    this.noResults = false;
+  }
+  onClickCaseStudies() {
+    this.loadAll = false;
+    this.loadEvents = false;
+    this.loadPress = false;
+    this.loadInsights = false;
+    this.loadCaseStudies = true;
+    this.noResults = true;
   }
 
 }
