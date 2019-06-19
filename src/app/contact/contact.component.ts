d62598338e4e21fe079e9d430dd2f53d0c03b140
YB <younes.bourakadi@gmail.com>
2019-06-19 16:03:40 +0100

    Fix formatting and add hp hash

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, FormControl } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: "app-contact",
-  templateUrl: "./contact.component.html",
-  styleUrls: ["./contact.component.scss"]
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-  duckUrl = "assets/img/aicura-duck.png";
-  teethUrl = "assets/img/aicura-teeth.png";
+  duckUrl = 'assets/img/aicura-duck.png';
+  teethUrl = 'assets/img/aicura-teeth.png';
   contactForm: FormGroup;
 
-  constructor(private form: FormBuilder, private http: HttpClient) {}
+  constructor(private form: FormBuilder, private http: HttpClient) { }
 
   public ngOnInit(): void {
     this.contactForm = new FormGroup({
@@ -26,7 +26,7 @@ export class ContactComponent implements OnInit {
   public submitForm() {
     const contactData = this.contactForm.getRawValue();
     // cloud function endpoint
-    this.http.post("", contactData).subscribe(
+    this.http.post('', contactData).subscribe(
       res => {
         console.log(res);
       },
