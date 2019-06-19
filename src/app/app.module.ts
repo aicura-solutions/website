54e3bf325124958c7ce4b22d38e09ab42718023a
YB <younes.bourakadi@gmail.com>
2019-06-19 15:13:04 +0100

    Add ng reactive form and post request in contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AicuraLogoOrangeComponent } from './aicura-logo-orange/aicura-logo-orange.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { MeetTheTeamComponent } from './meet-the-team/meet-the-team.component';
-import { AicuraLogoWhiteComponent } from './aicura-logo-white/aicura-logo-white.component';
-import { InsightsComponent } from './insights/insights.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ContactComponent } from './contact/contact.component';
-import { PoliciesComponent } from './policies/policies.component';
-import { ComingSoonComponent } from './coming-soon/coming-soon.component';
-import { OpenSourceBankingComponent } from './open-source-banking/open-source-banking.component';
-import { FrontEndAutomationComponent } from './front-end-automation/front-end-automation.component';
-import { NavigationToggleComponent } from './navigation-toggle/navigation-toggle.component';
-import { NavigationToggleWhiteComponent } from './navigation-toggle-white/navigation-toggle-white.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AicuraLogoOrangeComponent } from "./aicura-logo-orange/aicura-logo-orange.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { MeetTheTeamComponent } from "./meet-the-team/meet-the-team.component";
+import { AicuraLogoWhiteComponent } from "./aicura-logo-white/aicura-logo-white.component";
+import { InsightsComponent } from "./insights/insights.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { ContactComponent } from "./contact/contact.component";
+import { PoliciesComponent } from "./policies/policies.component";
+import { ComingSoonComponent } from "./coming-soon/coming-soon.component";
+import { OpenSourceBankingComponent } from "./open-source-banking/open-source-banking.component";
+import { FrontEndAutomationComponent } from "./front-end-automation/front-end-automation.component";
+import { NavigationToggleComponent } from "./navigation-toggle/navigation-toggle.component";
+import { NavigationToggleWhiteComponent } from "./navigation-toggle-white/navigation-toggle-white.component";
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
   declarations: [
     AppComponent,
@@ -38,9 +39,12 @@ import { NavigationToggleWhiteComponent } from './navigation-toggle-white/naviga
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,17 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, FormControl } from "@angular/forms";
+
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.scss"]
 })
 export class ContactComponent implements OnInit {
-  duckUrl = 'assets/img/aicura-duck.png';
-  teethUrl = 'assets/img/aicura-teeth.png';
+  duckUrl = "assets/img/aicura-duck.png";
+  teethUrl = "assets/img/aicura-teeth.png";
+  contactForm: FormGroup;
 
-  constructor() { }
+  constructor(private form: FormBuilder, private http: HttpClient) {}
 
-  ngOnInit() {
+  public ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      name: new FormControl(),
+      email: new FormControl(),
+      message: new FormControl()
+    });
   }
 
+  public submitForm() {
+    const contactData = this.contactForm.getRawValue();
+    // cloud function endpoint
+    this.http.post("", contactData).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+        console.log(err.error);
+        console.log(err.name);
+        console.log(err.email);
+        console.log(err.message);
+        console.log(err.status);
+      }
+    );
+    console.log(contactData);
+  }
 }
