40a085c044ec6d6fc67bd1789ae7eb767f113903
Dan <iamdanhenderson@gmail.com>
2019-06-19 09:01:11 +0100

    Contact page pre-working form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { HomePageComponent } from "./home-page/home-page.component";
-import { MeetTheTeamComponent } from "./meet-the-team/meet-the-team.component";
-import { InsightsComponent } from "./insights/insights.component";
-import { ContactComponent } from "./contact/contact.component";
-import { PoliciesComponent } from "./policies/policies.component";
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-import { ComingSoonComponent } from "./coming-soon/coming-soon.component";
-import { OpenSourceBankingComponent } from "./open-source-banking/open-source-banking.component";
-import { FrontEndAutomationComponent } from "./front-end-automation/front-end-automation.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MeetTheTeamComponent } from './meet-the-team/meet-the-team.component';
+import { InsightsComponent } from './insights/insights.component';
+import { ContactComponent } from './contact/contact.component';
+import { PoliciesComponent } from './policies/policies.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ComingSoonComponent } from './coming-soon/coming-soon.component';
+import { OpenSourceBankingComponent } from './open-source-banking/open-source-banking.component';
+import { FrontEndAutomationComponent } from './front-end-automation/front-end-automation.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent, pathMatch: 'full' },
   { path: 'meet-the-team', component: ComingSoonComponent },
   { path: 'insights', component: InsightsComponent },
-  { path: 'contact', component: ComingSoonComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'policies', component: PoliciesComponent },
   { path: 'community-projects', component: ComingSoonComponent },
   { path: 'open-source-banking', component: OpenSourceBankingComponent },
@@ -23,7 +23,7 @@ const routes: Routes = [
   { path: 'about', component: ComingSoonComponent },
   { path: 'our-team', component: ComingSoonComponent },
   { path: '**', component: PageNotFoundComponent }
-]
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
   duckUrl = 'assets/img/aicura-duck.png';
+  teethUrl = 'assets/img/aicura-teeth.png';
 
   constructor() { }
 
