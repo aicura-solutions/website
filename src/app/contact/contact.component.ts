7101d17e48fe105951e70259172ecf645a06e9f1
Dan <iamdanhenderson@gmail.com>
2019-06-25 10:57:19 +0100

    Minor cosmetic changes

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 import { HttpClient } from '@angular/common/http';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact',
@@ -10,9 +11,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ContactComponent implements OnInit {
   duckUrl = 'assets/img/aicura-duck.png';
+  teethUrl = 'assets/img/aicura-teeth.png';
   contactForm: FormGroup;
 
-  constructor(private form: FormBuilder, private http: HttpClient) {}
+  constructor(private form: FormBuilder, private http: HttpClient, title: Title) {
+    title.setTitle('Contact Us');
+  }
 
   public ngOnInit(): void {
     this.contactForm = new FormGroup({
@@ -43,6 +47,5 @@ export class ContactComponent implements OnInit {
           console.log('This is status error: ' + err.status);
         }
       );
-    console.log(contactData);
   }
 }
