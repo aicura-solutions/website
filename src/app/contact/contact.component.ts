03ab9fe591ea38ca26a14f129fea4ff18e4ed90f
YB <younes.bourakadi@gmail.com>
2019-06-25 12:17:33 +0100

    Add contact form validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators
+} from '@angular/forms';
 
 import { HttpClient } from '@angular/common/http';
 import { Title } from '@angular/platform-browser';
@@ -13,21 +18,44 @@ export class ContactComponent implements OnInit {
   duckUrl = 'assets/img/aicura-duck.png';
   teethUrl = 'assets/img/aicura-teeth.png';
   contactForm: FormGroup;
+  submitted = false;
 
-  constructor(private form: FormBuilder, private http: HttpClient, title: Title) {
+  constructor(
+    private form: FormBuilder,
+    private http: HttpClient,
+    title: Title
+  ) {
     title.setTitle('Contact Us');
   }
 
+  // public ngOnInit(): void {
+  //   this.contactForm = new FormGroup({
+  //     name: new FormControl(),
+  //     email: new FormControl(),
+  //     message: new FormControl()
+  //   });
+  // }
+
   public ngOnInit(): void {
-    this.contactForm = new FormGroup({
-      name: new FormControl(),
-      email: new FormControl(),
-      message: new FormControl()
+    this.contactForm = this.form.group({
+      name: [undefined, [Validators.required]],
+      email: [undefined, [Validators.required, Validators.email]],
+      message: [undefined, [Validators.required]]
     });
+  }
+
+  get f() {
+    return this.contactForm.controls;
   }
 
   public submitForm() {
     const contactData = this.contactForm.getRawValue();
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.contactForm.invalid) {
+      return;
+    }
 
     this.http
       .post(
@@ -40,11 +68,6 @@ export class ContactComponent implements OnInit {
         },
         err => {
           console.log(err);
-          console.log('This is general error: ' + err.error);
-          console.log('This is name error: ' + err.name);
-          console.log('This is email error: ' + err.email);
-          console.log('This is message error: ' + err.message);
-          console.log('This is status error: ' + err.status);
         }
       );
   }
