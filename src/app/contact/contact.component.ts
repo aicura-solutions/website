d7007df902829a0b81cc72a3a0a1a03908cb0f15
Dan Henderson <danhenderson95@users.noreply.github.com>
2019-06-26 15:48:12 +0100

    Feature/contact update (#28)
    * Add submitForm success message
    
    * Angular to alert messages

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
-  FormControl,
-  Validators
+  Validators,
+  FormGroupDirective
 } from '@angular/forms';
 
 import { HttpClient } from '@angular/common/http';
@@ -18,25 +18,13 @@ export class ContactComponent implements OnInit {
   duckUrl = 'assets/img/aicura-duck.png';
   teethUrl = 'assets/img/aicura-teeth.png';
   contactForm: FormGroup;
-  submitted = false;
+  formRef: FormGroupDirective;
 
-  constructor(
-    private form: FormBuilder,
-    private http: HttpClient,
-    title: Title
-  ) {
+  constructor(private form: FormBuilder, private http: HttpClient, title: Title) {
     title.setTitle('Contact Us');
   }
 
-  // public ngOnInit(): void {
-  //   this.contactForm = new FormGroup({
-  //     name: new FormControl(),
-  //     email: new FormControl(),
-  //     message: new FormControl()
-  //   });
-  // }
-
-  public ngOnInit(): void {
+  ngOnInit() {
     this.contactForm = this.form.group({
       name: [undefined, [Validators.required]],
       email: [undefined, [Validators.required, Validators.email]],
@@ -48,10 +36,12 @@ export class ContactComponent implements OnInit {
     return this.contactForm.controls;
   }
 
+  alert(message: string) {
+    window.alert(message);
+  }
+
   public submitForm() {
     const contactData = this.contactForm.getRawValue();
-    this.submitted = true;
-
     // stop here if form is invalid
     if (this.contactForm.invalid) {
       return;
@@ -64,10 +54,17 @@ export class ContactComponent implements OnInit {
       )
       .subscribe(
         res => {
-          console.log(res);
+          alert(`Your message has been submitted- we'll be in touch soon!`);
+          window.location.reload();
         },
         err => {
-          console.log(err);
+          if (err.status == 200) {
+            alert(`Your message has been submitted- we'll be in touch soon!`);
+            window.location.reload();
+          } else {
+            alert('There was a problem submitting your message, please try again.');
+            console.log(err);
+          }
         }
       );
   }
